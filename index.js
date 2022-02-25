ee18c8f21876444ce671dfceaac6095d0203bdb7
SDBoahen <SDBoahen@gmail.com>
2022-02-25 13:21:19 -0500

    We started a JS/HTML/CSS Code Base! YAY! ^_^

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log("👀")  //
+
+
+
+
+// LOGICAL/MEMORY/INTERNAL - JS
+let a = 1
+let b = 2
+let c = a + b
+// LOGICAL/MEMORY/INTERNAL - JS
+
+
+
+
+// THE "Vesica-Pices" LOGICAL x VISUAL - JS
+console.log( "This is c: " , c )
+// THE "Vesica-Pices" LOGICAL x VISUAL - JS
+
+
+
+
+// VISUAL/DOM/EVENT-HANDLING - JS
+
+// create elements
+// create elements
+// create elements
+// create elements
+// create elements
+// create elements
+// create elements
+// create elements
+// create elements
+// create elements
+// create elements
+// create elements
+// create elements
+
+// VISUAL/DOM/EVENT-HANDLING - JS
